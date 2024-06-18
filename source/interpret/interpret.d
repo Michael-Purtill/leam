0561808b0183e0295decb005945adc2d0e233548
Michael Purtill <michaelpurtill123@hotmail.com>
2024-06-18 01:15:52 -0300

    Convert interpreter to class, add a basic top-level symbol table. ID/Assignment evaluators

--- source/interpret/interpret.d
+++ source/interpret/interpret.d
@@ -21,94 +21,102 @@ template binCaseGenAdd(string operator) {
     (_1, _2) => throw new Exception("INVALID ARITHMETIC OPERATION"));
 }
 
-Literal evanLiteral(Expr expr) {
-  return expr.value;
-}
+class Evaluator {
+  Literal[string] symbolTable = []; // you evaluate expressions and add them to symbol tables
 
-Literal evalBinary(Expr expr) {
-  Expr left = expr.operands[0];
-  Expr right = expr.operands[1];
+  Literal interpret(Expr expr) {
+    ExprType type = expr.type;
 
-  Literal leftVal = interpret(left);
-  Literal rightVal = interpret(right);
-  Literal returnVal;
+    return type.match!(
+      (LiteralType _) => evanLiteral(expr),
+      (BinaryType _) => evalBinary(expr),
+      (UnaryType _) => evalUnary(expr),
+      (IDType _) => evalID(expr),
+      (AssignmentType _) => evalAssignment(expr)
 
-  switch (expr.operator.type) {
-  case TokenType.ADD:
-    mixin binCaseGenAdd!("+");
-    matchOperands(leftVal, rightVal);
-    return returnVal;
-    break;
-  case TokenType.SUB:
-    mixin binCaseGen!("-");
-    matchOperands(leftVal, rightVal);
-    return returnVal;
-    break;
-  case TokenType.MUL:
-    mixin binCaseGen!("*");
-    matchOperands(leftVal, rightVal);
-    return returnVal;
-    break;
-  case TokenType.DIV:
-    mixin binCaseGen!("/");
-    matchOperands(leftVal, rightVal);
-    return returnVal;
-    break;
-  case TokenType.AND:
-    mixin binCaseGen!("&&");
-    matchOperands(leftVal, rightVal);
-    return returnVal;
-    break;
-  case TokenType.OR:
-    mixin binCaseGen!("||");
-    matchOperands(leftVal, rightVal);
-    return returnVal;
-    break;
-  default:
-    throw new Exception("FAILED TO PARSE BINARY EXPRESSION");
-  }
-}
-
-Literal evalUnary(Expr expr) {
-  Literal value = interpret(expr.operands[0]);
-  Literal retVal;
-  if (expr.operator.type == TokenType.SUB) {
-    value.match!(
-      (int v) => retVal = -1 * v,
-      (_) => throw new Exception("ILLEGAL UNARY OPERATION: + applied to non-number")
     );
-
-    return retVal;
-  }
-  else if (expr.operator.type == TokenType.NOT) {
-    value.match!(
-      (bool v) => retVal = !v,
-      (_) => throw new Exception("ILLEGAL UNARY OPERATION: `not` applied to non-bool")
-    );
-
-    return retVal;
   }
 
-  throw new Exception("ERROR INTERPRETING UNARY EXPRESSION");
-}
+  Literal evanLiteral(Expr expr) {
+    return expr.value;
+  }
 
-Literal evalID(Expr expr) {
-  
-}
+  Literal evalBinary(Expr expr) {
+    Expr left = expr.operands[0];
+    Expr right = expr.operands[1];
 
-Literal evalAssignment(Expr expr) {
+    Literal leftVal = interpret(left);
+    Literal rightVal = interpret(right);
+    Literal returnVal;
 
-}
+    switch (expr.operator.type) {
+    case TokenType.ADD:
+      mixin binCaseGenAdd!("+");
+      matchOperands(leftVal, rightVal);
+      return returnVal;
+      break;
+    case TokenType.SUB:
+      mixin binCaseGen!("-");
+      matchOperands(leftVal, rightVal);
+      return returnVal;
+      break;
+    case TokenType.MUL:
+      mixin binCaseGen!("*");
+      matchOperands(leftVal, rightVal);
+      return returnVal;
+      break;
+    case TokenType.DIV:
+      mixin binCaseGen!("/");
+      matchOperands(leftVal, rightVal);
+      return returnVal;
+      break;
+    case TokenType.AND:
+      mixin binCaseGen!("&&");
+      matchOperands(leftVal, rightVal);
+      return returnVal;
+      break;
+    case TokenType.OR:
+      mixin binCaseGen!("||");
+      matchOperands(leftVal, rightVal);
+      return returnVal;
+      break;
+    default:
+      throw new Exception("FAILED TO PARSE BINARY EXPRESSION");
+    }
+  }
 
-Literal interpret(Expr expr) {
-  ExprType type = expr.type;
+  Literal evalUnary(Expr expr) {
+    Literal value = interpret(expr.operands[0]);
+    Literal retVal;
+    if (expr.operator.type == TokenType.SUB) {
+      value.match!(
+        (int v) => retVal = -1 * v,
+        (_) => throw new Exception("ILLEGAL UNARY OPERATION: + applied to non-number")
+      );
 
-  return type.match!(
-    (LiteralType _) => evanLiteral(expr),
-    (BinaryType _) => evalBinary(expr),
-    (UnaryType _) => evalUnary(expr),
-    (IDType _) => evalID(expr),
-    (AssignmentType _) => evalAssignment(expr)
+      return retVal;
+    }
+    else if (expr.operator.type == TokenType.NOT) {
+      value.match!(
+        (bool v) => retVal = !v,
+        (_) => throw new Exception("ILLEGAL UNARY OPERATION: `not` applied to non-bool")
+      );
 
-  );
+      return retVal;
+    }
+
+    throw new Exception("ERROR INTERPRETING UNARY EXPRESSION");
+  }
+
+  Literal evalID(Expr expr) {
+    return symbolTable[expr.value];
+  }
+
+  Literal evalAssignment(Expr expr) {
+    Expr right = expr.operands[1];
+    Literal rightVal = interpret(right);
+    symbolTable[expr.operands[0].value] = rightVal;
+
+    return rightVal;
+  }
 }
